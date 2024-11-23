eb23b6145c06660f55e93b4b0af1f9f1c217822f
Haritch-6509650757 <haritch.utc@dome.tu.ac.th>
2024-11-23 14:57:56 +0700

    Update docker data

--- docker_sql/api/connection.php
+++ docker_sql/api/connection.php
@@ -7,9 +7,4 @@ $dbname = "cs361project";
 
 $conn = mysqli_connect($servername, $username, $password, $dbname);
 
-if ($conn) {
-    echo "connected";
-} else {
-    echo "not connected: " . mysqli_connect_error();
-}
 ?>
