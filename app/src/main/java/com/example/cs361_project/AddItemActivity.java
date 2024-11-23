043f0d443a5347166a2f26dda2aa2b8b2edb030d
wuttipat6509650716 <wuttipat.pip@dome.tu.ac.th>
2024-11-24 01:19:11 +0700

    add svg menu and arrow back

---
+++ app/src/main/java/com/example/cs361_project/AddItemActivity.java
@@ -0,0 +1,31 @@
+package com.example.cs361_project;
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class AddItemActivity extends AppCompatActivity {
+
+    SharedPreferences sharedPreferences;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.add_item_page);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        sharedPreferences = getSharedPreferences("MyPrefs", MODE_PRIVATE);
+
+
+    } //end OnCreate
+}
