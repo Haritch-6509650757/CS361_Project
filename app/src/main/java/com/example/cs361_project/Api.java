35d582722284393f81516bc07a6ce1d0114338a3
Korakrit6509650203 <korakrit.pon@dome.tu.ac.th>
2024-11-23 20:20:39 +0700

    add ui profile and editprofile

---
+++ app/src/main/java/com/example/cs361_project/Api.java
@@ -0,0 +1,10 @@
+package com.example.cs361_project;
+
+public class Api {
+    public static final String IPV4 = "10.0.2.2";
+    public static final String URL_GET_USERS = "http://" + IPV4 + ":8080/api/getusers.php";
+    public static final String URL_REGISTER = "http://" + IPV4 + ":8080/api/register.php";
+    public static final String URL_LOGIN = "http://" + IPV4 + ":8080/api/login.php";
+    public static final String URL_PROFILE = "http://" + IPV4 + ":8080/api/profile.php";
+    public static final String URL_EDIT_PROFILE = "http://" + IPV4 + ":8080/api/edit_profile.php";
+}
