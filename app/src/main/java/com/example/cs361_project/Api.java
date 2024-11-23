05b04daf311d51dbd7be7699cbc36e97f2523bf4
Haritch-6509650757 <haritch.utc@dome.tu.ac.th>
2024-11-23 14:51:39 +0700

    Get products from mysql and modify api

---
+++ app/src/main/java/com/example/cs361_project/Api.java
@@ -0,0 +1,4 @@
+package com.example.cs361_project;
+
+public class Api {
+}
---
+++ docker_sql/api/getproducts.php
@@ -0,0 +1,20 @@
+<?php
+include 'connection.php';
+
+if ($conn) {
+    $sql = "SELECT * FROM products";
+    $result = mysqli_query($conn, $sql);
+
+    if ($result) {
+        $response = [];
+        while ($row = mysqli_fetch_assoc($result)) {
+            $response[] = $row;
+        }
+        echo json_encode($response, JSON_PRETTY_PRINT);
+    } else {
+        echo "Query error: " . mysqli_error($conn);
+    }
+} else {
+    echo "Connection failed: " . mysqli_connect_error();
+}
+?>
