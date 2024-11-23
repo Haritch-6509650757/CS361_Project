0a8468bb6392896f106224d39faf7e796e45d41c
Haritch-6509650757 <haritch.utc@dome.tu.ac.th>
2024-11-24 00:43:17 +0700

    Update API

--- app/src/main/java/com/example/cs361_project/Api.java
+++ app/src/main/java/com/example/cs361_project/Api.java
@@ -1,4 +1,6 @@
 package com.example.cs361_project;
 
 public class Api {
+    public static final String IPV4 = "10.0.2.2";
+    public static final String URL_GET_PRODUCTS = "http://" + IPV4 + ":8080/api/getproducts.php";
 }
