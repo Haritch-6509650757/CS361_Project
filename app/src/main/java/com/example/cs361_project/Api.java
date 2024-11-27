a87661572bc0f7cafd9d9a8c9187603bf4c9564b
Haritch-6509650757 <haritch.utc@dome.tu.ac.th>
2024-11-28 00:04:49 +0700

    Add get post api

--- app/src/main/java/com/example/cs361_project/Api.java
+++ app/src/main/java/com/example/cs361_project/Api.java
@@ -4,4 +4,6 @@ public class Api {
     public static final String IPV4 = "10.0.2.2";
     public static final String URL_GET_PRODUCTS = "http://" + IPV4 + ":8080/api/getproducts.php";
     public static final String URL_GET_MERCHANT = "http://" + IPV4 + ":8080/api/getmerchant.php";
+    public static final String URL_BUYITEMS = "http://" + IPV4 + ":8080/api/buyitems.php";
+    public static final String URL_BUYMERCHANT = "http://" + IPV4 + ":8080/api/buymerchant.php";
 }
